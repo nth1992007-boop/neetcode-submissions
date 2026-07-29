7e40ffa2c6f403da30e9ec7a7d7acb9440858906
nth1992007-boop <nth1992007@gmail.com>
2026-07-29 22:14:04 +0700

    Add: largest-rectangle-in-histogram - submission-0

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int max_area = 0;
+        int max_height = 0;
+
+        for(int height : heights){
+            max_height = max(max_height, height);
+        }
+
+        for(int current_height = 0; current_height <= max_height; current_height++){
+            int current_area = 0;
+
+            for(int j = 0; j < heights.size(); j++){
+                if(heights[j] >= current_height){
+                    current_area += current_height;
+                    max_area = max(max_area, current_area);
+                }else{
+                    current_area = 0;
+                }
+            }
+        }
+        return max_area;
+    }
+};
