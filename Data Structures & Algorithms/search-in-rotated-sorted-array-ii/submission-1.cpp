1feb27db379b6a13f69b9bd380725cc8927d0614
nth1992007-boop <nth1992007@gmail.com>
2026-08-02 00:04:12 +0700

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target){
+                return true;
+            }else if(target == nums[left]){
+                return true;
+            }else if(target == nums[right]){
+                return true;
+            }
+
+            if(nums[left] == nums[mid] && nums[right] == nums[mid]){
+                left++;
+                right--;
+                continue;
+            }
+
+            if(nums[left] <= nums[mid] ){
+                if(nums[left] < target && target < nums[mid]){
+                    right = mid - 1; 
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && nums[right] > target){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
